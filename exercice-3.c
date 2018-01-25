a2fc79745bb428a04332546422b6633cae9915ae
sguilly <stephane.guilly@laposte.net>
2018-01-25 21:29:06 +0100

    Create exercice-3.c

---
+++ exercice-3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{
+	int n, i, T[50], T1[50], T2[50];
+	
+	printf("Entrer la taille du tableau : ");
+	scanf("%d",&n);
+	
+    for(i=0; i<n; i++){
+   		printf("Entrer l'element %d :", i+1);
+   		scanf("%d",&T[i]);
+	}  
+	
+	/* VOTRE CODE ICI */
+	
+	printf("\nAffichage du tableau saisi (T) : \n");
+	
+  /* VOTRE CODE ICI */
+	
+	printf("\n\nAffichage du tableau des nombres pairs (T1) : \n");
+	
+  /* VOTRE CODE ICI */
+  
+	printf("\nAffichage du tableau des nombres impairs (T2) : \n");
+	
+  /* VOTRE CODE ICI */
+  
+}
